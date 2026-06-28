64ecad0577b6bbc872b081b7fb95756575dacd35
Jack Cho <jack06215@gmail.com>
2026-06-28 17:10:09 +0900

    fix meetingbar_send_notification

--- dot_config/zsh/src/meetingbar.zsh
+++ dot_config/zsh/src/meetingbar.zsh
@@ -2,8 +2,8 @@ function meetingbar_send_notification() {
   local json_file="$1"
 
   source "$HOME/.zshrc.local"
-  source "$XDG_CONFIG_HOME/zsh/src/zsh_python_init.zsh"
-  source "$XDG_CONFIG_HOME/zsh/src/darwin_pre_init.zsh"
-  cd "$XDG_CONFIG_HOME/zsh/src/python" || return 1
+  source "$ZDOTDIR/src/zsh_python_init.zsh"
+  source "$ZDOTDIR/src/darwin_pre_init.zsh"
+  cd "$ZDOTDIR/src/python" || return 1
   command "$ZSH_PYTHON_BIN" -m meetingbar.read_json --json_path "$json_file"
 }
